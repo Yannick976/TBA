27b06316303f3128213b1d20c4b697de3617e218
yannick-prat <yannick.prat@edu.esiee.fr>
2024-12-09 16:04:25 +0100

    Create item.py

---
+++ item.py
@@ -0,0 +1,19 @@
+class Item:
+    def __init__(self, name, description, weight):
+        """
+        Initialise un nouvel objet Item.
+
+        :paramètre name: str, le nom de l'objet
+        :paramètre description: str, la description de l'objet
+        :paramètre weight: float, le poids de l'objet
+        """
+        self.name = name
+        self.description = description
+        self.weight = weight
+
+    def __str__(self):
+        """
+        Retourne une représentation textuelle de l'objet.
+        """
+        return f"Item(name='{self.name}', description='{self.description}', weight={self.weight})"
+      
