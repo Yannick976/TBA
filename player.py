1d0a819e530064c163ed64998fca307917244d54
yannick-prat <yannick.prat@edu.esiee.fr>
2024-12-09 17:07:41 +0100

    Update player.py

--- player.py
+++ player.py
@@ -6,6 +6,7 @@ class Player():
         self.name = name
         self.current_room = None
         self.history = []  # Historique des pièces visitées
+        self.inventory = dict()
     
     # Define the move method.
     def move(self, direction):
